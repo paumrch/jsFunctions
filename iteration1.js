dddb35044bd28623d348f5eef8993bd9650ccae1
Pau March <paumarch@MacBook-Pro-de-Pau.local>
2021-11-29 21:38:01 +0100

    Iteration1Done

---
+++ iteration1.js
@@ -0,0 +1,15 @@
+//Iteración #1: Buscar el máximo**
+
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne, numberTwo) {
+
+  return Math.max(numberOne, numberTwo);
+  
+}
+
+
+console.log(sum(5, 4));
+
+
+
